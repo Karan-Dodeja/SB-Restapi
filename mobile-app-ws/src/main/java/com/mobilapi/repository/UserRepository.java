b1ff6849036b1261c71c5b47c43d9820db2afdd2
kdodeja62@gmail.com <kdodeja62@gmail.com>
2023-04-27 04:44:43 +0530

    Created UserRepo for DB Connection with application

---
+++ mobile-app-ws/src/main/java/com/mobilapi/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.mobilapi.repository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mobilapi.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+	// UserEntity findUserByEmai(String email);
+}
--- mobile-app-ws/src/main/java/com/mobilapi/service/UserServiceImpl.java
+++ mobile-app-ws/src/main/java/com/mobilapi/service/UserServiceImpl.java
@@ -1,16 +1,29 @@
 package com.mobilapi.service;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mobilapi.dto.UserDto;
+import com.mobilapi.entity.UserEntity;
+import com.mobilapi.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+	@Autowired
+	private UserRepository userRepo;
+	
 	@Override
 	public UserDto createUser(UserDto user) {
-		// TODO Auto-generated method stub
-		return null;
+		UserEntity userEntity = new UserEntity();
+		BeanUtils.copyProperties(user, userEntity);
+		userEntity.setUserId("testUserId");
+		userEntity.setEncryptedPassword("test");
+		UserEntity storedUserdetails = userRepo.save(userEntity);
+		UserDto returnValue = new UserDto();
+		BeanUtils.copyProperties(storedUserdetails, returnValue);
+		return returnValue;
 	}
 	
 }
